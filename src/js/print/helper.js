8b9636308dec1980eb28cee70ad5fdf8ae226c03
Kerem Zopcuk <keremzopcuk@gmail.com>
2021-09-13 22:09:09 +0300

    Init date-fns package

--- src/js/print/helper.js
+++ src/js/print/helper.js
@@ -1,5 +1,7 @@
 import { toDosWrapper } from '../mainHelpers.js';
 import { toDoArray } from '../db/db.js';
+import { formatDistanceToNow } from 'date-fns';
+import { de } from 'date-fns/locale';
 
 function clearMainWrapper(toDos) {
   return (toDosWrapper.innerHTML = `
@@ -30,7 +32,9 @@ function printItems(toDo) {
         <div class="card-inhalt d-flex flex-column mx-10">
           <div class="dinge-kopf d-flex align-items-center">
             <div class="dinge-kopf-links d-flex align-items-center">
-              <button class="check-button btn btn-rounded mr-lg-20 mr-sm-10" data-todo-check-btn-id="${toDo.id}"></button>
+              <button class="check-button btn btn-rounded mr-lg-20 mr-sm-10" data-todo-check-btn-id="${
+                toDo.id
+              }"></button>
             </div>
             <div class="dinge-kopf-rechts d-flex flex-column">
               <div class="dinge-kopf-rechts-oben">
@@ -56,9 +60,14 @@ function printItems(toDo) {
             </p>
           </div>
           <!-- Change Class d-none to d-flex -->
-          <div class="dinge-fuss d-none justify-content-between" data-fuss-id="${toDo.id}">
+          <div class="dinge-fuss d-none justify-content-between" data-fuss-id="${
+            toDo.id
+          }">
             <div class="dinge-fuss-links m-5">
-              <span class="dinge-erstelltungs-datum text-muted font-size-12">Erstellt: 12.10.2021 12:21</span>
+              <span class="dinge-erstelltungs-datum text-muted font-size-12">${formatDistanceToNow(
+                new Date(toDo.id),
+                { locale: de, addSuffix: true }
+              )} erstellt.</span>
             </div>
             <div class="dinge-fuss-mitte m-5">
               <span class="badge badge-pill">/ ${toDo.ordner}</span>
@@ -66,7 +75,9 @@ function printItems(toDo) {
             <div class="dinge-fuss-rechts m-5">
               <div class="ding-buttons">
                 <div class="btn-group" role="group" aria-label="Basic example">
-                  <a href="#" class="btn" onclick="halfmoon.toggleModal('modal-todo')" data-todo-edit-btn="${toDo.id}">
+                  <a href="#" class="btn" onclick="halfmoon.toggleModal('modal-todo')" data-todo-edit-btn="${
+                    toDo.id
+                  }">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#0000ff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                       <path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34"></path>
                       <polygon points="18 2 22 6 12 16 8 16 8 12 18 2"></polygon>
